315514f507102a910d90f1da1892c00364193712
Steve Peters-Luciani <50124723+spetersluciani@users.noreply.github.com>
2022-02-23 23:05:21 -0600

    Created Recipe models
    Created the models needed for the Recipe object for mongodb.

---
+++ models/recipe.js
@@ -0,0 +1,24 @@
+const { defaultConfiguration } = require('express/lib/application');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ingredientSchema = new Schema({
+    measurement: Number,
+    units: String,
+    name: String
+});
+
+const recipeSchema = new Schema({
+    cooktime: String, 
+    cookingMethod: String,
+    prepTime: String,
+    recipeCategory: String,
+    recipeCuisine: String,
+    recipeIngredients: [ingredientSchema],
+    recipeInstructions: [String],
+    recipeYield: Number
+});
+
+const Recipe = mongoose.model('recipe', recipeSchema);
+
+module.exports = Recipe;
