08e3a0104a71e00fb771574960cace8f1b4431b1
Steve Peters-Luciani <50124723+spetersluciani@users.noreply.github.com>
2022-03-01 15:10:22 -0600

    Setup DB connections

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const { application } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+require('dotenv').config();
 
 const app = express();
 
 const port = process.env.port || 5000;
+
+mongoose
+    .connect(process.env.DB, { useNewUrlParser: true })
+    .then(() => console.log(`Database connected successfully`))
+    .catch((err) => console.log(err));
+
+mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -11,8 +21,13 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.send('Welcome to Express');
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    next();
 });
 
 app.listen(port, () => {
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const { redirect } = require('express/lib/response');
 const router = express.Router();
 const Recipe = require('../models/recipe');
 
-router.get('/recipe/:category', (req, res, next) => {
-    Recipe.find({recipeCategory: req.params.category})
+router.get('/recipe', (req, res, next) => {
+    Recipe.find({})
+        .then((data) => res.json(data))
+        .catch(next);
+});
+
+router.get('/recipe/category/:category', (req, res, next) => {
+    Recipe.find({"recipeCategory": req.params.category})
+        .then((data) => res.json(data))
+        .catch(next);
+});
+
+router.get('/recipe/cuisine/:cuisine', (req, res, next) => {
+    Recipe.find({"recipeCuisine": req.params.cuisine})
         .then((data) => res.json(data))
         .catch(next);
 });
@@ -17,7 +28,7 @@ router.get('/recipe/:id', (req, res, next) => {
 
 router.post('/recipe', (req, res, next) => {
     if (req.body.recipe) {
-        Recipe.create(req.body)
+        Recipe.create(req.body.recipe)
             .then((data) => res.json(data))
             .catch(next);
     }
@@ -28,3 +39,5 @@ router.delete('/recipe/:id', (req, res, next) => {
         .then((data) => res.json(data))
         .catch(next);
 });
+
+module.exports = router;
