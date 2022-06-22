0c94fd67b4168580d63a110cc882f588aff5353b
Steve Peters-Luciani <50124723+spetersluciani@users.noreply.github.com>
2022-06-21 23:30:22 -0500

    Implemented swagger and recipeCategory
    Added the recipeCategory model to the API.
    
    Implemented Swagger for API documentation.

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
 const Recipe = require('../models/recipe');
+const RecipeCategory = require('../models/recipeCategory');
+
+router.use('/documentation', swaggerUI.serve);
+router.get('/documentation', swaggerUI.setup(swaggerDoc));
 
 router.get('/recipe', (req, res, next) => {
     Recipe.find({})
@@ -36,6 +42,24 @@ router.post('/recipe', (req, res, next) => {
 
 router.delete('/recipe/:id', (req, res, next) => {
     Recipe.findOneAndDelete({_id: req.params.id})
+        .then((data) => res.json(data))
+        .catch(next);
+});
+
+router.get('/recipeCategory', (req, res, next) => {
+    RecipeCategory.find({})
+        .then((data) => res.json(data))
+        .catch(next);
+});
+
+router.post('/recipeCategory', (req, res, next) => {
+    RecipeCategory.create(req.body.recipeCategory)
+        .then((data) => res.json(data))
+        .catch(next);
+});
+
+router.delete('/recipeCategory/:id', (req, res, next) => {
+    RecipeCategory.findOneAndDelete({_id: req.params.id})
         .then((data) => res.json(data))
         .catch(next);
 });
