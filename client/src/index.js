a2c12f1f82aa136cf6283bc156032a8e5ae1843d
Steve Peters-Luciani <50124723+spetersluciani@users.noreply.github.com>
2022-06-22 22:13:10 -0500

    Reset React back to default
    Restructuring the application and decided it was best to just reset React to stock.

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import { Home, Category, Recipe } from './pages';
+import {App} from './containers/App/App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/category/:category' element={<Category />} />
-        <Route path='/recipe/:recipe' element={<Recipe />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- client/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Home } from './Home';
-export { default as Category } from './Category';
-export { default as Recipe } from './Recipe';
