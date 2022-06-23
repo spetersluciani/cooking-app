5fe6218e44b3d29a6acb65abc5bb048d3859cfb6
Steve Peters-Luciani <50124723+spetersluciani@users.noreply.github.com>
2022-06-23 14:40:38 -0500

    Added Links to LatestRecipe and PopularRecipe
    Added Links to LatestRecipe and PopularRecipe that now leverage the Recipes page.

--- client/src/components/LatestRecipes/LatestRecipes.js
+++ client/src/components/LatestRecipes/LatestRecipes.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 export const LatestRecipes = ({recipes}) => {
     const sortedRecipes = recipes.sort((a,b) => new Date(b.dateCreated) - new Date(a.dateCreated)).slice(0,5);
@@ -8,7 +9,7 @@ export const LatestRecipes = ({recipes}) => {
             <h2>Latest Recipes</h2>
             <div className="recipeList">
                 {sortedRecipes.map((recipe, index) => (
-                    <p key={index}>{recipe.name}</p>
+                    <p key={index}><Link to={`/recipe/${recipe._id}`}>{recipe.name}</Link></p>
                 )
                 )}
             </div>
--- client/src/components/PopularRecipes/PopularRecipes.js
+++ client/src/components/PopularRecipes/PopularRecipes.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const PopularRecipes = ({recipes}) => {
     const filteredRecipes = recipes.sort((a,b) => b.views - a.views).slice(0,5);
@@ -8,7 +9,7 @@ export const PopularRecipes = ({recipes}) => {
             <h2>Popular Recipes</h2>
             <div className="recipeList">
                 {filteredRecipes.map((recipe, index) => (
-                    <p key={index}>{recipe.name}</p>
+                    <p key={index}><Link to={`/recipe/${recipe._id}`}>{recipe.name}</Link></p>
                 )
                 )}
             </div>
