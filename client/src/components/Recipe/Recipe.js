970922badae4b5d779268d948e853bba5edf93e8
Steve Peters-Luciani <50124723+spetersluciani@users.noreply.github.com>
2022-07-05 20:26:00 -0500

    Updated Recipe.js render
    Updated Recipe.js to render other fields from the fetched recipe.

--- client/src/components/Recipe/Recipe.js
+++ client/src/components/Recipe/Recipe.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 export const Recipe = () => {
 
-    const [currentRecipe, setCurrentRecipe] = useState({});
+    const [currentRecipe, setCurrentRecipe] = useState();
     const { recipeId } = useParams();
 
     useEffect(() => {
@@ -14,10 +14,30 @@ export const Recipe = () => {
         });
     }, []);
 
-    return (
-        <Fragment>
-            <h1>Recipe</h1>
-            <h2>{currentRecipe.name}</h2>
-        </Fragment>
-    )
+    if (currentRecipe !== undefined) {
+        return (
+            <div>
+                <h1>Recipe</h1>
+                <h2>{currentRecipe.name}</h2>
+                <p>Cook Time: {currentRecipe.cooktime}</p>
+                <p>Prep Time: {currentRecipe.prepTime}</p>
+                <p>Yield: {currentRecipe.recipeYield} serving{currentRecipe.recipeYield > 1 && 's'}</p>
+                <p>Recipe Category: {currentRecipe.recipeCategory}</p>
+                <p>Ingredients</p>
+                <ul>
+                {currentRecipe.recipeIngredients.map((ingredient, index) => {
+                    return <li key={index}>{ingredient.measurement} {ingredient.units} {ingredient.name}</li>
+                })}
+                </ul>
+                <p>Instructions</p>
+                <ol>
+                {currentRecipe.recipeInstructions.map((instruction, index) => {
+                    return <li key={index}>{instruction}</li>
+                })}
+                </ol>
+            </div>
+        )
+    } else {
+        return <p>Loading...</p>
+    }
 }
