898a0a021dd9e70bb8c58073286175c7b0764120
Steve Peters-Luciani <50124723+spetersluciani@users.noreply.github.com>
2022-07-12 12:45:43 -0500

    Added styling to GlobalNavigation

--- client/src/components/GlobalNavigation/GlobalNavigation.jsx
+++ client/src/components/GlobalNavigation/GlobalNavigation.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import './GlobalNavigation.css';
 
 export const GlobalNavigation = () => {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/create">Create Recipe</Link>
+        <nav className="global-navigation">
+            <NavLink className="global-link" activeClassName="active" to="/">Home</NavLink>
+            <NavLink className="global-link" activeClassName="active" to="/create">Create Recipe</NavLink>
         </nav>
     )
 }
