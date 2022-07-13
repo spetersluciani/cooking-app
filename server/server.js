d9a9bcaffc86378b4840383ad1b57aed4b482093
Steve Peters-Luciani <50124723+spetersluciani@users.noreply.github.com>
2022-07-13 10:39:13 -0500

    Modified for support for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.port || 9000;
+const port = process.env.PORT || 9000;
 
 mongoose
     .connect(process.env.DB, { useNewUrlParser: true })
